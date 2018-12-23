bca6419caa121057dc686ac829a3bdff771073df
Nek <nekanek@protonmail.com>
2018-12-22 20:17:08 -0500

    day 11 part 2

--- java/src/Day11.java
+++ java/src/Day11.java
@@ -2,35 +2,45 @@ import java.awt.*;
 
 
 public class Day11 {
-  final static short size = 300;
-  private static Short[][] grid = new Short[size][size];
+  private final static short GRID_SIZE = 300;
+  private static Short[][] grid = new Short[GRID_SIZE][GRID_SIZE];
 
   public static void main(String[] args) {
-    Point p = getPowerfullCell();
+    Point p = getPowerfullCell((short)3, (short)3);
     System.out.println("Max Power at: " + p.x + "," + p.y);
+
+    // part 2
+    // TODO: brute forcing, memoize already calculated power for smaller square sizes
+    Point p2 = getPowerfullCell((short)1, GRID_SIZE);
+    System.out.println("Max Power at: " + p2.x + "," + p2.y);
   }
 
-  private static Point getPowerfullCell() {
-    short maxPower = 0;
+  private static Point getPowerfullCell(short startSize, short endSize) {
+    int maxPower = 0;
+    int maxSize = -1;
     Point maxPowerCoordinates = new Point();
 
-    for (short x = 0; x < size; x++) {
-      for(short y = 0; y < size; y++) {
-        short powerAt = getPowerAt(x, y);
-        if (powerAt > maxPower) {
-          maxPower = powerAt;
-          maxPowerCoordinates = new Point(x + 1, y + 1);
+    for (short size = startSize; size <= endSize; size++) {
+      for (short x = 0; x < GRID_SIZE; x++) {
+        for(short y = 0; y < GRID_SIZE; y++) {
+          int powerAt = getPowerAt(x, y, size);
+          if (powerAt > maxPower) {
+            maxPower = powerAt;
+            maxSize = size;
+            maxPowerCoordinates = new Point(x + 1, y + 1);
+          }
         }
       }
     }
+    System.out.println("maxSize:" + maxSize);
     return maxPowerCoordinates;
   }
 
-  private static short getPowerAt(short x, short y) {
-    short sum = 0;
-    for (short i = x; i < x + 3; i++) {
-      for (short j = y; j < y + 3; j++) {
-        if (i >= size || j >= size) {
+  private static int getPowerAt(short x, short y, short size) {
+    int sum = 0;
+    for (short i = x; i < x + size; i++) {
+      for (short j = y; j < y + size; j++) {
+        if (i >= GRID_SIZE || j >= GRID_SIZE) {
           return 0;
         }
         sum += getCellPower(i, j);
